49d6ff2861ff3ce457534a17454d4c12a6ff4d91
WaterpigCZ <49160555+WaterpigCZ@users.noreply.github.com>
2020-02-13 14:22:23 +0100

    Uploaded update a.2
    Changes:
    - Added Roomba enemies
    - Added Health
    - Updated jump script
    
    Found bugs:
    - You can sometimes get stuck in roombas, forcing them to fly (Mostly a movement script problem, maybe unfixable with my current knowledge)

---
+++ MartinsAdventuresSourceCode a.2/Assets/Scripts/Enemys/Roomba/roomba_ctrl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomba_ctrl : MonoBehaviour
+{
+    [SerializeField] private Transform m_PlayerCheck;
+    [SerializeField] float playerRadius;
+    [SerializeField] private LayerMask m_WhatIsPlayer;
+    [SerializeField] private Transform m_Player;
+    [SerializeField] float movement_speed;
+    [SerializeField] SpriteRenderer e_RoombaSprite;
+    [SerializeField] Animator e_RoombaAnimator;
+
+    bool inRadius;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Tests for player in radius
+        bool wasGrounded = inRadius;
+        inRadius = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_PlayerCheck.position, playerRadius, m_WhatIsPlayer);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                inRadius = true;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        // Makes roomba move towards player
+        // Help my friend's house is getting haunted
+        if (inRadius==true)
+        {
+            if (m_Player.position.x - transform.position.x <= 0)
+            {
+                transform.position = new Vector2(transform.position.x - movement_speed, transform.position.y);
+                e_RoombaSprite.flipX = false;
+            }
+            else if (m_Player.position.x - transform.position.x >= 0)
+            {
+                transform.position = new Vector2(transform.position.x + movement_speed, transform.position.y);
+                e_RoombaSprite.flipX = true;
+            }
+            e_RoombaAnimator.Play("mainanim", -1, (1f / 2) * 1);
+        }
+        else
+        {
+            //animator.Play ("AnimationName", 1, ( 1f / total_frames_in_animation ) * desired_frame);
+            e_RoombaAnimator.Play("mainanim", -1, (1f / 2) * 2);
+            
+        }
+    }
+}
---
+++ MartinsAdventuresSourceCode a.2/Assets/Scripts/UI/UI_ctrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ctrl : MonoBehaviour
+{
+    [SerializeField] Text ui_text_p_Health;
+    [SerializeField] GameObject p_Player;
+    int ui_p_Health;
+    string ui_p_HealthString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ui_p_Health = martin_ctrl.p_Health;
+        ui_p_HealthString = ui_p_Health.ToString();
+        ui_text_p_Health.text = ui_p_HealthString;
+    }
+}
