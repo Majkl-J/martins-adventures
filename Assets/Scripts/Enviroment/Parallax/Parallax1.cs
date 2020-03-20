5061e8c8d559e51ca4d3de14ac3dd27870297efb
Waterpig <49160555+WaterpigCZ@users.noreply.github.com>
2020-03-20 15:32:48 +0100

    Miniupdate a.4.0-pre1
    Parallax background, new textures

---
+++ Assets/Scripts/Enviroment/Parallax/Parallax1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax1 : MonoBehaviour
+{
+    public float scrollSpeed;
+    public GameObject martin;
+    Vector2 startPos;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        startPos = martin.transform.position;
+        float newpos = Mathf.Repeat(Time.time * scrollSpeed, 16);
+        transform.position = startPos + Vector2.right * newpos;
+    }
+}
